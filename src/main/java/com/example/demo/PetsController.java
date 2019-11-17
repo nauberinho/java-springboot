8dcc4d3369d73209eafadfee32a2822a89b41602
Niklas Nauber <nauber91@hotmail.com>
2019-11-17 16:21:55 +0100

    Adds incomplete config

--- src/main/java/com/example/demo/PetsController.java
+++ src/main/java/com/example/demo/PetsController.java
@@ -10,37 +10,59 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.validation.Valid;
 import java.util.List;
 
+import org.springframework.context.annotation.ComponentScan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+
+
 @RestController
 @RequestMapping("/pets")
+@ComponentScan({"main.java.com.example.demo.repositories.PetsRepository"})
 public class PetsController {
-  @Autowired
   private PetsRepository repository;
-}
 
-@RequestMapping(value = "/", method = RequestMethod.GET)
-public List<Pets> getAllPets() {
-  return repository.findAll();
-}
-@RequestMapping(value = "/{id}", method = RequestMethod.GET)
-public Pets getPetById(@PathVariable("id") ObjectId id) {
-  return repository.findBy_id(id);
-}
+  
 
-@RequestMapping(value = "/{id}", method = RequestMethod.PUT)
-public void modifyPetById(@PathVariable("id") ObjectId id, @Valid 
-@RequestBody Pets pets) {
-  pets.set_id(id);
-  repository.save(pets);
-}
+  public static void main(String[] args) {
+    SpringApplication.run(PetsController.class, args);
+  }
 
-@RequestMapping(value = "/", method = RequestMethod.POST)
-public Pets createPet(@Valid @RequestBody Pets pets) {
-  pets.set_id(ObjectId.get());
-  repository.save(pets);
-  return pets;
-}
+  @RequestMapping("/")
+    String home() {
+        return "Hello World!";
+    }
 
-@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
-public void deletePet(@PathVariable ObjectId id) {
-  repository.delete(repository.findBy_id(id));
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public List<Pets> getAllPets() {
+    return repository.findAll();
+    }
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public Pets getPetById(@PathVariable("id") ObjectId id) {
+    return repository.findBy_id(id);
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.PUT)
+    public void modifyPetById(@PathVariable("id") ObjectId id, @Valid 
+    @RequestBody Pets pets) {
+    pets.set_id(id);
+    repository.save(pets);
+    }
+
+    @RequestMapping(value = "/", method = RequestMethod.POST)
+    public Pets createPet(@Valid @RequestBody Pets pets) {
+    pets.set_id(ObjectId.get());
+    repository.save(pets);
+    return pets;
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+    public void deletePet(@PathVariable ObjectId id) {
+    repository.delete(repository.findBy_id(id));
+    }
+
+
 }
